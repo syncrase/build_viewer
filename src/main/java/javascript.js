90aff37e06dee2e9322a70544b804ed9e6f207ff
Your Name <you@example.com>
2015-05-26 10:08:50 +0200

    Replace the table to set up colors and states. This one works well

--- src/main/java/javascript.js
+++ src/main/java/javascript.js
@@ -23,5 +23,16 @@
  */
 
 alert("hello");
+        document.getElementsByTagName('body').defineProperties();
+        var table = document.getElementById(tableId);
+//        <!--// Create an empty <tr> element and add it to the 1st position of the table:-->
+        var row = table.insertRow(table.rows.length - 2);
+//        <!--// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:-->
+var cell = row.insertCell(0);
 
+cell.ins
+
+
+
+        
 
